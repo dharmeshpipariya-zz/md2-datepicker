557a8d1c78de636a400315f8822c8646742ec098
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-06-16 10:21:03 +0530

    removed source files.

--- src/app/components/datepicker/datepicker.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-import {Md2Datepicker} from '../../../components/datepicker/datepicker';
-
-@Component({
-  selector: 'datepicker-demo',
-  templateUrl: './app/components/datepicker/datepicker.html',
-  directives: [Md2Datepicker]
-})
-export class DatepickerComponent { }
